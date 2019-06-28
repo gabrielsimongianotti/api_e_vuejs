e11c9f836c567928753e9e1b83c42bcfb42bf54a
unknown <gabrielgianotti@outlook.com>
2019-06-28 11:07:38 -0300

    vue-router adicionado

--- projeto/src/main.js
+++ projeto/src/main.js
@@ -1,11 +1,29 @@
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
 
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
 
-Vue.use(VueMaterial)
+import Frequencias from './components/Frequencias.vue'
+import Solicitacoes from './components/Solicitacoes.vue'
+import Medico from './components/Medico.vue'
+import Paciente from './components/Paciente.vue'
+
 Vue.config.productionTip = false
+Vue.use(VueMaterial)
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/frequencias', component: Frequencias },
+  { path: '/solicitacoes', component: Solicitacoes },
+  { path: '/medico', component: Medico },
+  { path: '/paciente', component: Paciente }
+]
+
+const router = new VueRouter({
+  routes
+})
 
 Vue.material.registerTheme('default', {
   primary: 'blue',
@@ -15,6 +33,6 @@ Vue.material.registerTheme('default', {
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  router,
   render: h => h(App)
-})
+}).$mount('#app')
