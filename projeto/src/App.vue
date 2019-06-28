f02122b0944d9bf887aae71849287869c3a52ecd
unknown <gabrielgianotti@outlook.com>
2019-06-28 10:55:18 -0300

    vue-material adicionado

--- projeto/src/App.vue
+++ projeto/src/App.vue
@@ -1,28 +1,71 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+  <main class="phone-viewport">
+    <md-toolbar>
+      <md-button class="md-icon button" @click.native="toggleLeftSidenav">
+        <md-icon>menu</md-icon>
+      </md-button>
+
+      <h2 class="md-title">{{ title }}</h2>
+    </md-toolbar>
+
+    <md-sidenav class="md-left" ref="leftSidenav" @open="open('Left')" @close="close('Left')">
+      <md-toolbar>
+        <div class="md-toolbar-container">
+          <h3 class="md-title">Navegação</h3>
+        </div>
+      </md-toolbar>
+
+      <md-list @click.native="closeLeftSidenav">
+        <md-list-item>
+          <router-link to="/paciente">Paciente</router-link>
+        </md-list-item>
+        <md-list-item>
+          <router-link to="/medico">Médico</router-link>
+        </md-list-item>
+        <md-list-item>
+          <router-link to="/solicitacoes">Solicitações</router-link>
+        </md-list-item>
+        <md-list-item>
+          <router-link to="/frequencias">Frequências</router-link>
+        </md-list-item>
+      </md-list>
+   </md-sidenav>
+
+   <md-row>
+     <router-view></router-view>
+   </md-row>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'SistemaPlanoDeSaude',
+
+  data () {
+    return {
+      title: 'Sistema de plano de saude'
+    }
+  },
+
+  methods: {
+    toggleLeftSidenav () {
+      this.$refs.leftSidenav.toggle()
+    },
+
+    closeLeftSidenav () {
+      this.$refs.leftSidenav.close()
+    },
+
+    open (ref) {
+      console.log('Menu aberto: ' + ref)
+    },
+
+    close (ref) {
+      console.log('Menu fechado: ' + ref)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- projeto/src/main.js
+++ projeto/src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App'
 
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.css'
+
+Vue.use(VueMaterial)
 Vue.config.productionTip = false
+
+Vue.material.registerTheme('default', {
+  primary: 'blue',
+  accent: 'red',
+  warn: 'red'
+})
 
 /* eslint-disable no-new */
 new Vue({
